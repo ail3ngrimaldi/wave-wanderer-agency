c2648401f81ab9238e12b6755e9c20cad7fdae3f
Ailén Grimaldi <63558201+ail3ngrimaldi@users.noreply.github.com>
2025-12-23 22:56:05 -0300

    Update AdminDashboard.tsx

--- src/pages/AdminDashboard.tsx
+++ src/pages/AdminDashboard.tsx
@@ -37,7 +37,7 @@ const AdminDashboard = () => {
       return;
     }
 
-    const link = `${window.location.origin}/paquete/${id}`;
+    const link = `${window.location.origin}/paquete/${pkg.slug}`;
     setGeneratedLink(link);
 
     toast({
